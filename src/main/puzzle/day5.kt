dc164e438bd2c04c2a138f85a40885d094a725dc
Tony Seng <t.seng@dental-monitoring.com>
2020-12-08 15:07:41 +0100

    Day 5 part 2

--- src/main/puzzle/day5.kt
+++ src/main/puzzle/day5.kt
@@ -6,13 +6,34 @@ import kotlin.math.min
 
 fun main() {
     val inputs: List<String> = File("src/main/resources/input/5.txt").readLines()
+    part1(inputs)
+    part2(inputs)
+}
+
+fun part1(inputs: List<String>) {
     val result = inputs.map {
         calculateRow(it.take(7)) * 8 + calculateColumn(it.takeLast(3))
     }.maxOrNull()
     println("Part 1 = $result")
 }
 
-fun calculateRow(rowCharacter : String) : Int {
+fun part2(inputs: List<String>) {
+    var seatGrid : Array<Array<Int?>> = Array(128) { arrayOfNulls(8) }
+    inputs.forEach {
+        val i = calculateRow(it.take(7))
+        val j = calculateColumn(it.takeLast(3))
+        seatGrid[i][j] = 1
+    }
+    for (i in 0..127) {
+        for(j in 0..7) {
+            if(seatGrid[i][j] == null) {
+                println("Row: $i and column: $j and seatId = ${i * 8 + j}")
+            }
+        }
+    }
+}
+
+fun calculateRow(rowCharacter: String) : Int {
     var start = 0
     var end = 128
     for(i in rowCharacter.indices) {
