97f9bb716725ccb1ccf851cb36eb5bf1a3b8c4f0
Tony Seng <t.seng@dental-monitoring.com>
2020-12-09 14:55:30 +0100

    Add Day 7 and Day 8 solutions

---
+++ src/main/puzzle/Day7.kt
@@ -0,0 +1,47 @@
+package main.puzzle
+
+import java.io.File
+
+fun main() {
+  part1()
+  part2()
+}
+
+private val bagsRules = File("src/main/resources/input/7.txt")
+    .readLines()
+    .associate { line ->
+      val (bag, contents) = line.split(" bags contain ")
+      val contentList = contents.split(", ")
+          .mapNotNull { s ->
+            val (n, adj, color) = s.split(' ')
+            n.toIntOrNull()?.let { it to "$adj $color" }
+          }
+      bag to contentList
+    }
+
+private const val MY_BAG = "shiny gold"
+
+private fun part1() {
+  val outerBags = mutableSetOf<String>()
+  val bagQueue = ArrayDeque<String>()
+  bagQueue.add(MY_BAG)
+  while (bagQueue.isNotEmpty()) {
+    val currentBag = bagQueue.removeLast()
+    bagsRules.filterValues { bags ->
+      bags.any { it.second == currentBag }
+    }.keys.forEach { bag ->
+          if (outerBags.add(bag))
+            bagQueue.add(bag)
+        }
+  }
+  println("Part1: ${outerBags.size}")
+}
+
+private fun part2() {
+  fun bagsContainedIn(bag:String):Int =
+      (bagsRules[bag] ?: error("")).sumBy {
+        (n, innerBag) -> n + ( n * bagsContainedIn(innerBag) )
+      }
+
+  println("Part2: ${bagsContainedIn(MY_BAG)}")
+}
---
+++ src/main/puzzle/Day8.kt
@@ -0,0 +1,47 @@
+package main.puzzle
+
+import java.io.File
+
+fun main() {
+  var executed: MutableSet<Int> = mutableSetOf()
+  part1(instructions, executed)
+  part2(executed)
+}
+
+private val instructions = File("src/main/resources/input/8.txt").readLines()
+    .map {
+       instruction -> instruction.split(" ")
+    }.toMutableList()
+
+private fun part1(instructions: List<List<String>>, executed: MutableSet<Int>) : Boolean {
+  executed.clear()
+  var instructionPointer = 0
+  var accumulator = 0
+  while(!executed.contains(instructionPointer) && instructionPointer < instructions.size) {
+    executed.add(instructionPointer)
+    val (ops, args) = instructions[instructionPointer]
+    when(ops) {
+      "acc" -> { accumulator += args.toInt()
+        instructionPointer++ }
+      "jmp" -> instructionPointer += args.toInt()
+      "nop" -> instructionPointer++
+    }
+  }
+  println("Part1 = $accumulator")
+  return instructionPointer == instructions.size
+}
+
+private fun part2(executed: MutableSet<Int>) {
+  var originalProgram = instructions
+  for (i in executed.size - 2 downTo 0) {
+    val lastExecuted = executed.elementAt(i)
+    if (originalProgram[lastExecuted][0] == "jmp") {
+      originalProgram[lastExecuted] = listOf("nop", originalProgram[lastExecuted][1])
+      part1(originalProgram, executed)
+      if (part1(originalProgram, executed)) {
+        println("Instructions ended successfully")
+        break
+      }
+    }
+  }
+}
